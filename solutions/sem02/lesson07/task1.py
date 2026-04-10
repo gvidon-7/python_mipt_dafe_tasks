ea70f1c9881a3ebbb093814c4dedc1ded671d287
gvidon-7 <aleksandrlarionov71@icloud.com>
2026-04-10 23:04:08 +0300

    Ларионов Б12-512
    Только 1-я задача к 7-му занятию

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,7 +13,93 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+    
+    valid_types = ("hist", "violin", "box")
+    if diagram_type not in valid_types:
+        raise ValueError
+    
+    picture = plt.figure(figsize=(12, 12))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    main = picture.add_subplot(grid[:-1, 1:])
+    left = picture.add_subplot(grid[:-1, 0], sharey=main)
+    low = picture.add_subplot(grid[-1, 1:], sharex=main)
+
+    main.scatter(abscissa, ordinates, color="m", alpha=0.7)
+
+    if diagram_type == "hist":
+
+        low.hist(
+                abscissa,
+                bins=75,
+                color="m",
+                density=True
+        )
+
+        left.hist(
+                ordinates,
+                bins=75,
+                color="m",
+                density=True,
+                orientation="horizontal",
+        )
+
+        low.invert_yaxis()
+        left.invert_xaxis()
+    
+    elif diagram_type == "violin":
+        v_left = left.violinplot(
+                ordinates, 
+                vert=False, 
+                showmedians=True
+        )
+
+        for body in v_left["bodies"]:
+            body.set_facecolor("m")
+            body.set_edgecolor("m")
+
+        for part in v_left:
+            if part != "bodies":
+                v_left[part].set_edgecolor("m")
+
+        v_low = low.violinplot(
+                abscissa, 
+                vert=True, 
+                showmedians=True
+        )
+
+        for body in v_low["bodies"]:
+            body.set_facecolor("m")
+            body.set_edgecolor("m")
+
+        for part in v_low:
+            if part != "bodies":
+                v_low[part].set_edgecolor("m")
+
+        low.invert_yaxis()
+        left.invert_xaxis()
+        
+    else:
+
+        left.boxplot(ordinates, vert=False, patch_artist=True,
+                     boxprops=dict(facecolor="m", alpha=0.7),
+                     medianprops=dict(color="darkmagenta"),
+                     whiskerprops=dict(color="m"),
+                     capprops=dict(color="m"),
+                     flierprops=dict(marker="o", markerfacecolor="m", markeredgecolor="m"))
+        
+        low.boxplot(abscissa, vert=True, patch_artist=True,
+                       boxprops=dict(facecolor="m", alpha=0.7),
+                       medianprops=dict(color="darkmagenta"),
+                       whiskerprops=dict(color="m"),
+                       capprops=dict(color="m"),
+                       flierprops=dict(marker="o", markerfacecolor="m", markeredgecolor="m"))
+        
+        low.invert_yaxis()
+        left.invert_xaxis()
+        
     pass
 
 

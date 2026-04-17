5b94d2465a0aba953cf3e3bea57260ab013e751e
gvidon-7 <aleksandrlarionov71@icloud.com>
2026-04-17 23:08:11 +0300

    Ларионов, Б12-512
    ДЗ полностью к 8-му уроку

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -8,27 +8,67 @@ from matplotlib.animation import FuncAnimation
 
 
 def create_modulation_animation(
-    modulation, 
-    fc, 
-    num_frames, 
-    plot_duration, 
-    time_step=0.001, 
-    animation_step=0.01,
-    save_path=""
+    modulation,  # Функция модуляции
+    fc,  # Частота несущего сигнала
+    num_frames,  # Количество кадров, которые будут сохранены в анимацию
+    plot_duration,  # длительность интервала времени до зацикливания
+    time_step=0.001,  # Промежуток времени между соседними вычисленными точками сигнала
+    animation_step=0.01,  # время, которое прибавляется к оси за кадр
+    save_path="",
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    figure, axis = plt.subplots(figsize=(16, 9))
+    axis.set_xlabel("Время (с)", fontsize=20)
+    axis.set_ylabel("Амплитуда", fontsize=20)
+    axis.text(
+        0.98,
+        0.98,
+        "Модулированный сигнал",
+        transform=axis.transAxes,
+        ha="right",
+        va="top",
+        fontsize=14,
+        bbox=dict(facecolor="white", alpha=0.5, edgecolor="black"),
+    )
+    x_values = np.arange(0, plot_duration, time_step)
+    if modulation is None:
+        modulation_multiplier = 1.0
+    else:
+        modulation_multiplier = modulation(x_values)
+    y_values_start = modulation_multiplier * np.sin(2 * np.pi * fc * x_values)
+    (line_start,) = axis.plot(x_values, y_values_start, lw=1, c="purple")
+
+    axis.set_xlim(x_values.min(), x_values.max())
+    axis.set_ylim(-1.5, 1.5)
+
+    def update_frame(frame_id: int):
+        t = x_values + animation_step * frame_id
+        if modulation is None:
+            modulation_multiplier = 1.0
+        else:
+            modulation_multiplier = modulation(t)
+        y_values_new = modulation_multiplier * np.sin(2 * np.pi * fc * t)
+        line_start.set_data(t, y_values_new)
+        axis.set_xlim(t.min(), t.max())
+        return line_start, axis
+
+    anim = FuncAnimation(figure, update_frame, frames=num_frames, interval=50, blit=False)
+
+    if save_path:
+        anim.save(save_path, writer="pillow")
+
+    return anim
 
 
 if __name__ == "__main__":
-    def modulation_function(t):
-        return np.cos(t * 6) 
 
-    num_frames = 100  
-    plot_duration = np.pi / 2 
-    time_step = 0.001  
-    animation_step = np.pi / 200 
-    fc = 50  
+    def modulation_function(t):
+        return np.cos(t * 6)
+
+    num_frames = 100
+    plot_duration = np.pi / 2
+    time_step = 0.001
+    animation_step = np.pi / 200
+    fc = 50
     save_path_with_modulation = "modulated_signal.gif"
 
     animation = create_modulation_animation(
@@ -38,6 +78,6 @@ if __name__ == "__main__":
         plot_duration=plot_duration,
         time_step=time_step,
         animation_step=animation_step,
-        save_path=save_path_with_modulation
+        save_path=save_path_with_modulation,
     )
     HTML(animation.to_jshtml())
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -5,30 +5,91 @@ import numpy as np
 
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
+from matplotlib.colors import ListedColormap  # !!!
 
 
-
-
+# Пусть путь всегда существует
 def animate_wave_algorithm(
-    maze: np.ndarray, 
-    start: tuple[int, int], 
-    end: tuple[int, int], 
-    save_path: str = ""
-)  -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
+) -> FuncAnimation:
+    starting_maze = maze.copy()
+
+    def make_visual(p_ways):
+        visual = np.where(starting_maze == 0, 0, 1)
+        visual[p_ways >= 0] = 2
+        return visual
+
+    p_ways = np.full_like(maze, -1)
+    coordinates = [start]
+    steps = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    head = 0
+    p_ways[start] = 0
+
+    frames = [make_visual(p_ways)]
+    current_dist = 0
+
+    while head < len(coordinates):
+        dist = p_ways[coordinates[head]]
+        if dist > current_dist:
+            frames.append(make_visual(p_ways))
+            current_dist = dist
+
+        maze[coordinates[head]] = 7
+        for i in range(len(steps)):
+            shift_up_down = coordinates[head][0] + steps[i][0]
+            shift_left_right = coordinates[head][1] + steps[i][1]
+            if ((0 <= shift_left_right < maze.shape[1]) == False) or (
+                (0 <= shift_up_down < maze.shape[0]) == False
+            ):
+                continue
+            if maze[shift_up_down, shift_left_right] == 1:
+                coordinates.append((shift_up_down, shift_left_right))
+                p_ways[shift_up_down, shift_left_right] = p_ways[coordinates[head]] + 1
+        head += 1
+
+    figure, axes = plt.subplots(figsize=(12, 12))
+    cmap = ListedColormap(["green", "brown", "blue"])
+    image = axes.imshow(
+        frames[0],
+        cmap=cmap,
+    )
+    axes.set_xticks(np.arange(starting_maze.shape[1]))
+    axes.set_yticks(np.arange(starting_maze.shape[0]))
+    axes.set_xticklabels(np.arange(starting_maze.shape[1]))
+    axes.set_yticklabels(np.arange(starting_maze.shape[0]))
+
+    axes.set_xticks(np.arange(-0.5, starting_maze.shape[1], 1), minor=True)
+    axes.set_yticks(np.arange(-0.5, starting_maze.shape[0], 1), minor=True)
+
+    axes.grid(which="minor", color="black", linestyle="-", linewidth=2)
+    axes.grid(which="major", visible=False)
+
+    axes.set_title("Волновой алгоритм", fontsize=16)
+
+    def update(frame_id):
+        image.set_data(frames[frame_id])
+        return [image]
+
+    anim = FuncAnimation(figure, update, frames=len(frames), interval=100, blit=True, repeat=False)
+
+    if save_path:
+        anim.save(save_path, writer="pillow")
+
+    return anim
+
 
 if __name__ == "__main__":
-    # Пример 1
-    maze = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])
+    maze = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
 
     start = (2, 0)
     end = (5, 0)
@@ -36,10 +97,8 @@ if __name__ == "__main__":
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     HTML(animation.to_jshtml())
-    
-    # Пример 2
-    
-    maze_path = "./data/maze.npy"
+
+    """ maze_path = "./data/maze.npy"
     loaded_maze = np.load(maze_path)
 
     # можете поменять, если захотите запустить из других точек
@@ -48,6 +107,4 @@ if __name__ == "__main__":
     loaded_save_path = "loaded_labyrinth.gif"
 
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
-    HTML(loaded_animation.to_jshtml())
-    
-    
+    HTML(loaded_animation.to_jshtml()) """
